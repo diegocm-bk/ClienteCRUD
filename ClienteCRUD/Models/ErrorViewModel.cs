a9d28fc1716669c56ae3ab90f1df3dbe95a31407
Diego <holorey212@gmail.com>
2025-08-17 15:57:37 -0500

    Initial commit

---
+++ ClienteCRUD/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ClienteCRUD.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
